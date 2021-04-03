3ebe5c65201cf9415d84f9fc16724f65c903479d
Scaldy <45393189+Scaldy@users.noreply.github.com>
2021-04-03 10:46:57 -0300

    primer commit

---
+++ Platzigram/urls.py
@@ -0,0 +1,13 @@
+"""Platzigram URLs module."""
+
+from django.urls import path
+
+from platzigram import views
+
+
+urlpatterns = [
+
+    path('hello-world/', views.hello_world),
+    path('hi/', views.hi),
+
+]
